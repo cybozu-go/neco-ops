4bdfa9b652a98fdfe68c9cd2d7317e7ba6ff3278
ymmt <ymmt2005@gmail.com>
2020-04-20 02:13:46 +0000

    Revert cert-manager CR versions to v1alpha2

--- test/cert-manager_test.go
+++ test/cert-manager_test.go
@@ -45,7 +45,7 @@ func testCertManager() {
 			issuerName = "self-signed-issuer"
 		}
 		certificate := fmt.Sprintf(`
-apiVersion: cert-manager.io/v1alpha3
+apiVersion: cert-manager.io/v1alpha2
 kind: Certificate
 metadata:
   name: test-certificate
