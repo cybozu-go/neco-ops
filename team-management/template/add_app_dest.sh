4072c6101adfa54754de0f575cb3771ece8920b8
Daichi Sakaue <daichi-sakaue@cybozu.co.jp>
2021-04-23 08:08:03 +0000

    reflect comments
    Signed-off-by: Daichi Sakaue <daichi-sakaue@cybozu.co.jp>

--- team-management/template/add_app_dest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# usage: ./add_app.sh app destination branch
-yq eval ". * {\"apps\": {\"$1\": {\"destinations\": {\"$2\": \"$3\"}}}}" settings.json -j > /tmp/settings.json
-mv /tmp/settings.json settings.json
-make format-settings > /dev/null
