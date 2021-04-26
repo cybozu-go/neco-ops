7ee9b948269e261257841ae7ad8d530384e0687d
Daichi Sakaue <daichi-sakaue@cybozu.co.jp>
2021-04-26 09:26:02 +0000

    reflect comments 2
    Signed-off-by: Daichi Sakaue <daichi-sakaue@cybozu.co.jp>

--- team-management/template/add_app.sh
+++ team-management/template/add_app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# usage: ./add_app.sh app team repo destination branch
-yq eval ". * {\"apps\": {\"$1\": {\"team\": \"$2\", \"repo\": \"$3\", \"destinations\": {\"$4\": \"$5\"}}}}" settings.json -j > /tmp/settings.json
+# usage: ./add_app.sh app team repo
+yq eval ". * {\"apps\": {\"$1\": {\"team\": \"$2\", \"repo\": \"$3\"}}}" settings.json -j > /tmp/settings.json
 mv /tmp/settings.json settings.json
 make format-settings > /dev/null
